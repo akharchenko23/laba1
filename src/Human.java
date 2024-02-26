4c6a5b86744c5b1d0cfb17c08f3ffad592077460
Tymko <kondrashko13@proton.me>
2024-02-26 20:47:56 +0200

    Розширив клас людини

--- src/Human.java
+++ src/Human.java
@@ -1,6 +1,49 @@
+//This class describes a human being.
+import Enums.Sex;
+
 public class Human {
-    private int age;
-    public Human(){
-        this.age = 1;
+    protected String name;
+    protected Sex sex;
+    protected int age;
+
+    public Human(String name, Sex sex, int age){
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * Increases age of a human by 1 year
+     */
+    public void incAge(){
+        age++;
+    }
+
+    public String toString(){
+        return name + ". " + sex + ", " + age;
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,7 @@
+import Enums.Sex;
+
 public class Student extends Human{
+    public Student(String name, Sex sex, int age) {
+        super(name, sex, age);
+    }
 }
