4a944d4a5570120d4fdaea9738a68dd5c9329d10
Tymko <kondrashko13@proton.me>
2024-02-26 20:49:33 +0200

    Додав енам статей

---
+++ src/Enums/Sex.java
@@ -0,0 +1,16 @@
+package Enums;
+
+public enum Sex {
+    MALE, FEMALE, INTERSEX;
+
+    public String toString() {
+        switch (this.ordinal()) {
+            case 0:
+                return "Хлопець";
+            case 1:
+                return "Дівчина";
+            default:
+                return "Інтерсекс";
+        }
+    }
+}
