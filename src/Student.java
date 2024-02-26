556a8fee1e6eb474988dcd1624b81ca2fdde7730
akharchenko23 <akharchenko@kse.org.ua>
2024-02-26 23:04:09 +0200

    student class без сортувань

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,53 @@
 import Enums.Sex;
+
 @SuppressWarnings("unused")
-public class Student extends Human{
+//студент успадковує від людини поля ім'я, вік, стать. Має свої групу(у значенні спеціальність) та курс
+public class Student extends Human {
+
+    protected int course;
+    protected String group;
+
     public Student(String name, Sex sex, int age) {
         super(name, sex, age);
+
     }
+
+    public Student(String name, Sex sex, int age, int course, String group) {
+        super(name, sex, age);
+        this.course = course;
+        this.group = group;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    /**
+     * Increases the course of a student by 1 year till 4
+     */
+    public void incCourse() {
+        if (course < 4) {
+            course++;
+        } else {
+            // TODO придумати як реалізуємо випускника
+        }
+
+    }
+
+    public String toString() {
+        return name + ". " + sex + ", " + age + " | " + course + ", " + group;
+    }
+
 }
